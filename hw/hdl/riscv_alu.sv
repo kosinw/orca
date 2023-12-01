735e0120896f5a2017d0a46d0342745f178443b1
Kosi Nwabueze <kosinw41@gmail.com>
2023-11-30 21:26:22 -0500

    finished alu

--- hw/hdl/riscv_alu.sv
+++ hw/hdl/riscv_alu.sv
@@ -11,6 +11,12 @@ module riscv_alu(
     output logic    [31:0]   result_out,
     output logic             branch_taken_out
 );
+    logic signed [31:0] a_sign;
+    logic signed [31:0] b_sign;
+
+    assign a_sign = a_in;
+    assign b_sign = b_in;
+
     always_comb begin
         branch_taken_out = `OFF;
 
@@ -18,24 +24,24 @@ module riscv_alu(
             `ALU_FUNC_ADD:      result_out = a_in + b_in;
             `ALU_FUNC_SUB:      result_out = a_in - b_in;
             `ALU_FUNC_SLL:      result_out = (a_in << b_in[4:0]);
-            `ALU_FUNC_SLT:      result_out = {31'b0,$signed(a_in)<$signed(b_in)};
-            `ALU_FUNC_SLTU:     result_out = {31'b0,$unsigned(a_in)<$unsigned(b_in)};
+            `ALU_FUNC_SLT:      result_out = {31'b0,a_sign<b_sign};
+            `ALU_FUNC_SLTU:     result_out = {31'b0,a_in<b_in};
             `ALU_FUNC_XOR:      result_out = a_in ^ b_in;
-            `ALU_FUNC_SRL:      result_out = (a_in >> b_in[4:0]);
-            `ALU_FUNC_SRA:      result_out = (a_in >>> b_in[4:0]);
+            `ALU_FUNC_SRL:      result_out = (a_sign >> b_in[4:0]);
+            `ALU_FUNC_SRA:      result_out = (a_sign >>> b_in[4:0]);
             `ALU_FUNC_OR:       result_out = a_in | b_in;
             `ALU_FUNC_AND:      result_out = a_in & b_in;
-            `ALU_FUNC_JALR:     result_out = {(a_in + b_in)[31:1], 1'b0};
+            `ALU_FUNC_JALR:     begin result_out = a_in+b_in; result_out = {result_out[31:1],1'b0}; end
             `ALU_FUNC_LUI:      result_out = {b_in[19:0],12'b0};
             `ALU_FUNC_BR:       begin
                 result_out = `ZERO;
                 case (br_func_in)
                     `BR_FUNC_EQ:    branch_taken_out = (a_in==b_in) ? `ON : `OFF;
                     `BR_FUNC_NE:    branch_taken_out = (a_in!=b_in) ? `ON : `OFF;
-                    `BR_FUNC_LT:    branch_taken_out = ($unsigned(a_in)<$unsigned(b_in)) ? `ON : `OFF;
-                    `BR_FUNC_GE:    branch_taken_out = ($signed(a_i)>=$signed(b_in)) ? `ON : `OFF;
-                    `BR_FUNC_LTU:   branch_taken_out = ($unsigned(a_in)<$unsigned(b_in)) ? `ON : `OFF;
-                    `BR_FUNC_GEU:   branch_taken_out = ($unsigned(a_in)>=$unsigned(b_in)) ? `ON : `OFF;
+                    `BR_FUNC_LT:    branch_taken_out = (a_sign<b_sign) ? `ON : `OFF;
+                    `BR_FUNC_GE:    branch_taken_out = (a_sign>=b_sign) ? `ON : `OFF;
+                    `BR_FUNC_LTU:   branch_taken_out = (a_in<b_in) ? `ON : `OFF;
+                    `BR_FUNC_GEU:   branch_taken_out = (a_in>=b_in) ? `ON : `OFF;
                     default:        branch_taken_out = `OFF;
                 endcase
             end
--- hw/sim/riscv_alu_tb.sv
+++ hw/sim/riscv_alu_tb.sv
@@ -4,7 +4,7 @@
 `include "hdl/riscv_constants.sv"
 
 module riscv_alu_tb;
-    logic [71:0] COMMANDS []
+    logic [71:0] COMMANDS [13:0];
 
     logic        clk_in;
     logic [3:0]  alu_func_in;
@@ -23,6 +23,23 @@ module riscv_alu_tb;
         .branch_taken_out(branch_taken_out)
     );
 
+    initial begin
+        COMMANDS[ 0] = {`ALU_FUNC_ADD,`BR_FUNC_NONE,32'd50,-32'd10};    // 50 + (-10) = 40
+        COMMANDS[ 1] = {`ALU_FUNC_SUB,`BR_FUNC_NONE,-32'd30,32'd40};    // -30 - 40 = -70
+        COMMANDS[ 2] = {`ALU_FUNC_SLL,`BR_FUNC_NONE,32'd256,32'd4};     // 256 << 4 = 4095
+        COMMANDS[ 3] = {`ALU_FUNC_SLT,`BR_FUNC_NONE,-32'd1,32'd9};      // -1 < 9 = T
+        COMMANDS[ 4] = {`ALU_FUNC_SLTU,`BR_FUNC_NONE,-32'd1,32'd9};     // -1 <u 9 = F
+        COMMANDS[ 5] = {`ALU_FUNC_XOR,`BR_FUNC_NONE,32'h5F07,32'h1467};
+        COMMANDS[ 6] = {`ALU_FUNC_SRL,`BR_FUNC_NONE,-32'd20,32'd2};
+        COMMANDS[ 7] = {`ALU_FUNC_SRA,`BR_FUNC_NONE,-32'd20,32'd2};
+        COMMANDS[ 8] = {`ALU_FUNC_OR,`BR_FUNC_NONE,32'h5F07,32'h1467};
+        COMMANDS[ 9] = {`ALU_FUNC_AND,`BR_FUNC_NONE,32'h5F07,32'h1467};
+        COMMANDS[10] = {`ALU_FUNC_JALR,`BR_FUNC_NONE,32'hC800,32'h3};
+        COMMANDS[11] = {`ALU_FUNC_LUI,`BR_FUNC_NONE,32'hC800,32'h3};
+        COMMANDS[12] = {`ALU_FUNC_BR,`BR_FUNC_GE,32'h0,32'h0};
+        COMMANDS[13] = {`ALU_FUNC_BR,`BR_FUNC_GEU,-32'h1,32'h8FCD};
+    end
+
     always begin
         #5;
         clk_in = !clk_in;
@@ -36,8 +53,8 @@ module riscv_alu_tb;
         clk_in = 0;
         #5;
 
-        for (integer i = 0; i <= 34; i = i + 1) begin
-            inst_in = INSTRUCTIONS[i];
+        for (integer i = 0; i <= 13; i = i + 1) begin
+            {alu_func_in,br_func_in,a_in,b_in} = COMMANDS[i];
             #10;
         end
 
