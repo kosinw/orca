db9e687e4391e9d8edce2b33dfbc078c19d804d1
Kosi Nwabueze <kosinw41@gmail.com>
2023-12-14 14:58:57 -0500

    conway

---
+++ sw/src/conway.c
@@ -0,0 +1,118 @@
+#include <runtime.h>
+
+#define BOARD_WIDTH 160
+#define BOARD_HEIGHT 45
+
+#define MAX_INITIAL_CELLS 15
+
+void
+initboard(bool *grid, int width, int height)
+{
+    int cells_left = MAX_INITIAL_CELLS;
+
+    for (int r = 0; r < height; r++) {
+        bool seen_in_row = false;
+
+        for (int c = 0; c < width; c++) {
+            if (!seen_in_row && cells_left > 0 && (entropy() % 100) < 2) {
+                grid[width*r+c] = true;
+                seen_in_row = true;
+                cells_left--;
+            } else {
+                grid[width*r+c] = false;
+            }
+        }
+    }
+}
+
+void
+updateboard(bool *grid, int width, int height)
+{
+    bool newgrid[width*height];
+    int dxy[] = {-1,0,+1};
+
+    for (int r = 0; r < height; r++) {
+        for (int c = 0; c < width; c++) {
+            int neighbors = 0;
+
+            // Count neighbors.
+            for (int x = 0; x < 3; x++) {
+                for (int y = 0; y < 3; y++) {
+                    int dx = dxy[x] + c;
+                    int dy = dxy[y] + r;
+
+                    if (dx == c && dy == r)
+                        continue;
+
+                    if (dx < 0 || dx >= width || dy < 0 || dy >= height)
+                        continue;
+
+                    if (grid[dx+dy*width]) {
+                        neighbors++;
+                    }
+                }
+            }
+
+            // Do rules
+            if (grid[c+r*width]) {
+                if (neighbors < 2) {
+                    newgrid[c+r*width] = false;
+                } else if (neighbors > 3) {
+                    newgrid[c+r*width] = false;
+                } else {
+                    newgrid[c+r*width] = true;
+                }
+            } else {
+                if (neighbors == 3) {
+                    newgrid[c+r*width] = true;
+                } else {
+                    newgrid[c+r*width] = false;
+                }
+            }
+        }
+    }
+
+    for (int i = 0; i < height*width; i++) {
+        grid[i] = newgrid[i];
+    }
+}
+
+void
+drawgrid(bool *grid, int width, int height)
+{
+    for (int r = 0; r < height; r++) {
+        for (int c = 0; c < width; c++) {
+            uint8_t color = !grid[width*r+c] ? (FOREGROUND_BLACK | BACKGROUND_WHITE) : (FOREGROUND_WHITE | BACKGROUND_BLACK);
+            videoputchar(0xdb, color, r, c);
+        }
+    }
+}
+
+void
+main(void)
+{
+    bool GRID[BOARD_WIDTH * BOARD_HEIGHT];
+
+    videoinit(FOREGROUND_WHITE | BACKGROUND_BLACK);
+    keyboard_wrctrl(); // flush the keyboard
+
+    videomovecursor(0, 0);
+
+    // Then sleep for like 2s
+    // nanosleep(2000000000);
+
+    // Initialize the board with random cells on.
+    initboard(GRID, BOARD_WIDTH, BOARD_HEIGHT);
+
+    while (1) {
+        // First update the game
+        updateboard(GRID, BOARD_WIDTH, BOARD_HEIGHT);
+
+        // Then draw the cells
+        // drawgrid(GRID, BOARD_WIDTH, BOARD_HEIGHT);
+
+        // Then sleep for like 300 ms
+        nanosleep(300000000);
+        break;
+    }
+}
